d4181ab811e996425e5353c041bf22b570195009
Vladislav Kheynov <lcastory86@gmail.com>
2021-10-18 00:15:22 +0700

    fix(lab_1)!: fix method by full refactoring

--- Lab1/src/main/kotlin/methods/Iterations.kt
+++ Lab1/src/main/kotlin/methods/Iterations.kt
@@ -1,6 +1,7 @@
 package methods
 
 import InitialData
+import InitialData.matrix
 import kotlin.math.abs
 
 fun iterations(): Array<Double> {
@@ -20,42 +21,78 @@ fun iterations(): Array<Double> {
         if (abs(matrix[i][i]) <= sum) throw Error("Diagonal dominance is not fulfilled in the provided matrix")
     }
 
-    val beta = b.clone()
-    for (i in 0 until n) {
-        beta[i] /= matrix[i][i]
-    }
+    val result = mutableListOf(0.0, 0.0, 0.0, 0.0)
+    val nextX = mutableListOf(0.0, 0.0, 0.0, 0.0)
 
-    val alpha = matrix.clone()
-    for (i in 0 until n) {
-        for (j in 0 until n) {
-            alpha[i][j] = if (i == j) 0.0 else -alpha[i][j] / matrix[i][i]
+    do {
+        for (i in matrix.indices) {
+            result[i] = nextX[i]
         }
-    }
-
-    var xs = beta.clone()
-    var iterations = 0
-    while (true) {
-        iterations++
-        val result = beta.clone()
-        for (i in 0 until n) {
-            var sum = 0.0
-            for (j in 0 until n) {
-                sum += (alpha[i][j] * xs[j])
+        for (i in matrix.indices) {
+            val beta = (b[i] / matrix[i][i])
+            var temp = 0.0
+            for (j in 0 until matrix[i].size) {
+                var newA = 0.0
+                if (i != j) {
+                    newA = -(matrix[i][j] / matrix[i][i])
+                }
+                temp += result[j] * newA
             }
-            result[i] += sum
+            nextX[i] = beta + temp
         }
-
-        var continueLoop = false
-        for (i in 0 until n) {
-            if (abs(result[i] - xs[i]) >= InitialData.eps) {
-                xs = result
-                continueLoop = true
-                break
-            }
-        }
-        if (!continueLoop) {
-            println("Iterations: $iterations")
-            return result
-        }
-    }
+    } while (validateEpsilon(
+            x = result,
+            nextX = nextX
+        )
+    )
+    return result.toTypedArray()
 }
+fun validateEpsilon(x: MutableList<Double>, nextX: MutableList<Double>): Boolean {
+    var iter = 0
+    for (i in x.indices) {
+        if (abs(x[i] - nextX[i]) >= InitialData.eps) {
+            return true
+        }
+        iter++
+    }
+    return iter != matrix.size
+}
+
+//val beta = b.clone()
+//for (i in 0 until n) {
+//    beta[i] /= matrix[i][i]
+//}
+//
+//val alpha = matrix.clone()
+//for (i in 0 until n) {
+//    for (j in 0 until n) {
+//        alpha[i][j] = if (i == j) 0.0 else -alpha[i][j] / matrix[i][i]
+//    }
+//}
+//
+//var xs = beta.clone()
+//var iterations = 0
+//while (true) {
+//    iterations++
+//    val result = beta.clone()
+//    for (i in 0 until n) {
+//        var sum = 0.0
+//        for (j in 0 until n) {
+//            sum += alpha[i][j] * xs[j]
+//        }
+//        result[i] += sum
+//    }
+//
+//    var continueLoop = false
+//    for (i in 0 until n) {
+//        if (abs(result[i] - xs[i]) >= InitialData.eps) {
+//            xs = result
+//            continueLoop = true
+//            break
+//        }
+//    }
+//    if (!continueLoop) {
+//        println("Iterations: $iterations")
+//        return result
+//    }
+//}
