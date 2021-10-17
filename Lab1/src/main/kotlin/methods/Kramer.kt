ec9b70c9c0e3a2841b46098a2e7b2f5fc593b2c0
Vladislav Kheynov <lcastory86@gmail.com>
2021-10-17 23:29:36 +0700

    feat(lab 1)!: change Int type to Double

--- Lab1/src/main/kotlin/methods/Kramer.kt
+++ Lab1/src/main/kotlin/methods/Kramer.kt
@@ -4,14 +4,17 @@ import det
 import replaceRow
 import transpose
 
-fun kramer(matrix: Array<Array<Int>>, b: Array<Int>): MutableList<Int> {
+fun kramer(): MutableList<Double> {
+    val matrix = InitialData.matrix
+    val b = InitialData.b
+
     val t = transpose(matrix)
     val d = det(matrix)
-    if (d == 0) throw Error("No solution because determinant is 0")
+    if (d == 0.0) throw Error("No solution because determinant is 0")
 
-    val result = mutableListOf<Int>()
+    val result = mutableListOf<Double>()
     for (i in 0 until t.size) {
-        result[i] = det(replaceRow(t, i, b)) / d
+        result.add(det(replaceRow(t, i, b)) / d)
     }
     return result
 }
