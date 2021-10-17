ed024ce445604e9a4281ba9230cd236415afb3e0
Vladislav Kheynov <lcastory86@gmail.com>
2021-10-18 00:16:02 +0700

    refactor(lab_1): change initialData to correct calculating

--- Lab1/src/main/kotlin/InitialData.kt
+++ Lab1/src/main/kotlin/InitialData.kt
@@ -1,10 +1,21 @@
 object InitialData {
     val matrix = arrayOf(
-        arrayOf(3.0, 20.0, -2.0, 0.0),
-        arrayOf(5.0, -4.0, 0.0, 20.0),
-        arrayOf(0.0, 5.0, 32.0, -3.0),
-        arrayOf(12.0, 0.0, 0.0, 3.0)
+        arrayOf(89.0, 10.0, 9.0, 6.0),
+        arrayOf(2.0, 21.0, 3.0, 4.0),
+        arrayOf(3.0, 4.0, 43.0, 23.0),
+        arrayOf(8.0, 13.0, 14.0, 59.0)
     )
     const val eps = 0.00001
-    val b = arrayOf(41.0, -19.0, 34.0, 29.0)
+    val b = arrayOf(1.0, 2.0, 3.0, 4.0)
 }
+
+/*
+arrayOf(
+arrayOf(3.0, 20.0, -2.0, 0.0),
+arrayOf(5.0, -4.0, 0.0, 20.0),
+arrayOf(0.0, 5.0, 32.0, -3.0),
+arrayOf(12.0, 0.0, 0.0, 3.0)
+)
+
+arrayOf(41.0, -19.0, 34.0, 29.0)
+*/
