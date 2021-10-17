25704b2bde65adbe0a4fbe022fad33c212028688
Vladislav Kheynov <lcastory86@gmail.com>
2021-10-17 20:06:32 +0700

    feat: added initial data storage object

---
+++ Lab1/src/main/kotlin/InitialData.kt
@@ -0,0 +1,10 @@
+object InitialData {
+    val matrix = arrayOf(
+        arrayOf(3, 20, -2, 0),
+        arrayOf(5, -4, 0, 20),
+        arrayOf(0, 5, 32, -3),
+        arrayOf(12, 0, 0, 3)
+    )
+    val eps = 0.00001
+    val b = arrayOf(41, -19, 34, 29)
+}
