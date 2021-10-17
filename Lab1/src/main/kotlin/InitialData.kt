430b8dd0b2f1164334f304dc2de9777d023154f2
Vladislav Kheynov <lcastory86@gmail.com>
2021-10-17 23:30:18 +0700

    feat(lab 1)!: change Int type to Double refactor(lab_1): add const modifier to 'epsilon'

--- Lab1/src/main/kotlin/InitialData.kt
+++ Lab1/src/main/kotlin/InitialData.kt
@@ -1,10 +1,10 @@
 object InitialData {
     val matrix = arrayOf(
-        arrayOf(3, 20, -2, 0),
-        arrayOf(5, -4, 0, 20),
-        arrayOf(0, 5, 32, -3),
-        arrayOf(12, 0, 0, 3)
+        arrayOf(3.0, 20.0, -2.0, 0.0),
+        arrayOf(5.0, -4.0, 0.0, 20.0),
+        arrayOf(0.0, 5.0, 32.0, -3.0),
+        arrayOf(12.0, 0.0, 0.0, 3.0)
     )
-    val eps = 0.00001
-    val b = arrayOf(41, -19, 34, 29)
+    const val eps = 0.00001
+    val b = arrayOf(41.0, -19.0, 34.0, 29.0)
 }
